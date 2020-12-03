f521b07813f9d234c7aa3cf7fc28f6cff0607026
gheoblink <72501874+gheoblink@users.noreply.github.com>
2020-12-04 00:20:49 +0300

    Add files via upload

---
+++ HW4OpenMP/cmake-build-debug/CMakeFiles/HW4OpenMP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW4OpenMP.dir/main.cpp.obj"
+  "HW4OpenMP.exe"
+  "HW4OpenMP.exe.manifest"
+  "HW4OpenMP.pdb"
+  "libHW4OpenMP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW4OpenMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
